261603697c69df163b6d84450ebb28af62ff000a
Tetsu Kinomura <tetsu99soccer@gmail.com>
2022-01-15 21:14:48 +0900

    :sparkles: Feat: 実行時間計測

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/t-kinomura/blockchain-go/blockchain"
+import (
+	"fmt"
+	"github.com/t-kinomura/blockchain-go/blockchain"
+	"time"
+)
 
 func main() {
 	bc := blockchain.NewBlockchain()
@@ -9,11 +13,15 @@ func main() {
 	bc.AddTransaction("transaction2")
 	bc.CreateBlock()
 
+	now := time.Now()
 	bc.AddTransaction("transaction3")
 	bc.AddTransaction("transaction4")
 	bc.CreateBlock()
+	ms := time.Since(now).Milliseconds()
 
 	bc.Print()
+
+	fmt.Printf("1ブロック生成にかかる時間: %vms\n", ms)
 }
 
 
